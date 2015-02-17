2569df5e12deba392a9ce83546c685fa2bc2fd2f
Jérémy Bethmont <jeremy.bethmont@gmail.com>
2015-02-17 14:03:27 +0100

    Handle dependencies in pip files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+parse_pip()
+{
+  pip_file="$1"
+  echo $pip_file
+
+  for i in `grep '^\s*\-r' $pip_file | sed s/^\s*\-r//`
+  do
+    dep="$(dirname $pip_file)/$i"
+    parse_pip "$dep"
+  done
+}
+
 if [ ! -e /venv/bin/python ]; then
     virtualenv /venv
 fi
@@ -12,7 +24,12 @@ if [ -z "$REQUIREMENTS_FILE" ]; then
     REQUIREMENTS_FILE="/app/requirements.txt"
 fi
 
-md5sum "$REQUIREMENTS_FILE" > /pipcache/new_sum.txt
+> /pipcache/new_sum.txt
+for f in `parse_pip "$REQUIREMENTS_FILE"`
+do
+  md5sum "$f" >> /pipcache/new_sum.txt
+done
+
 if ! cmp -s /pipcache/sum.txt /pipcache/new_sum.txt > /dev/null; then
     pip install --download /pipcache -r "$REQUIREMENTS_FILE"
     pip install --find-links /pipcache -r "$REQUIREMENTS_FILE"
